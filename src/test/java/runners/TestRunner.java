bf7427314ce508f753178d92f938386d0d5f5b37
Dikshaverma66 <dikshav410@gmail.com>
2025-08-03 16:11:11 +0530

    3/08/2025

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = {"/src/test/resources/features"},
         glue = {"stepdefinitions", "Hooks"},
-        plugin = {"pretty"
+        plugin = {"pretty", "html:target/cucumber-reports/html"
         }
 
 )
